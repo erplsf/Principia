6934f9821276d1f47f6f53aa09d387231ad4bbc4
Andriy Mykhaylyk <erp.lsf@gmail.com>
2021-02-18 15:31:29 +0100

    Add FlightPlan parameters setting

--- ksp_plugin_adapter/krpc/principia.cs
+++ ksp_plugin_adapter/krpc/principia.cs
@@ -6,7 +6,7 @@ namespace principia.ksp_plugin_adapter.krpc {
 
 [KRPCService (GameScene = GameScene.Flight)]
 public static class Principia {
-  private static readonly IntPtr plugin_;
+  internal static readonly IntPtr plugin_;
 
   static Principia() {
     plugin_ = ExternalInterface.Get().adapter_.Plugin();
@@ -34,6 +34,8 @@ public static class Principia {
       FlightPlanAdaptiveStepParameters prms =
           plugin_.FlightPlanGetAdaptiveStepParameters(vessel_guid);
       return new FlightPlanParameters(vessel_guid,
+                                      prms.integrator_kind,
+                                      prms.generalized_integrator_kind,
                                       prms.max_steps,
                                       prms.length_integration_tolerance,
                                       prms.speed_integration_tolerance);
@@ -45,26 +47,66 @@ public static class Principia {
 
 [KRPCClass (Service = "Principia")]
 public class FlightPlanParameters {
-  private string vessel_guid_;
+  private readonly string vessel_guid_;
+  private readonly long integrator_kind_;
+  private readonly long generalized_integrator_kind_;
+  private long max_steps_;
+  private double length_integration_tolerance_;
+  private double speed_integration_tolerance_;
 
   public FlightPlanParameters(string vessel_guid,
+                              long integrator_kind,
+                              long generalized_integrator_kind,
                               long max_steps,
                               double length_integration_tolerance,
                               double speed_integration_tolerance) {
     vessel_guid_ = vessel_guid;
-    this.MaxSteps = max_steps;
-    this.LengthIntegrationTolerance = length_integration_tolerance;
-    this.SpeedIntegrationTolerance = speed_integration_tolerance;
+    integrator_kind_ = integrator_kind;
+    generalized_integrator_kind_ = generalized_integrator_kind;
+    max_steps_ = max_steps;
+    length_integration_tolerance_ = length_integration_tolerance;
+    speed_integration_tolerance_ = speed_integration_tolerance;
+  }
+
+  private void Commit() {
+    Principia.plugin_.FlightPlanSetAdaptiveStepParameters(vessel_guid_,
+      new FlightPlanAdaptiveStepParameters{
+          integrator_kind = integrator_kind_,
+          generalized_integrator_kind = generalized_integrator_kind_,
+          max_steps = max_steps_,
+          length_integration_tolerance = length_integration_tolerance_,
+          speed_integration_tolerance = speed_integration_tolerance_
+      });
   }
 
   [KRPCProperty]
-  public long MaxSteps { get ; }
+  public double Tolerance {
+    get => length_integration_tolerance_;
+    set  {
+      if (value >= 1e-6 && value <= 1e6) {
+        length_integration_tolerance_ = value;
+        speed_integration_tolerance_ = value;
+        Commit();
+      } else {
+        throw new Exception("Tolerance is out of the supported range");
+      }
+    }
+  }
 
   [KRPCProperty]
-  public double LengthIntegrationTolerance { get ; }
+  public long MaxSteps {
+    get => max_steps_;
+    set {
+      if (value >= 100 && value <= long.MaxValue / factor) {
+        max_steps_ = value;
+        Commit();
+      } else {
+        throw new Exception("Max steps is out of the supported range");
+      }
+    }
+  }
 
-  [KRPCProperty]
-  public double SpeedIntegrationTolerance { get ; }
+  private const int factor = 4;
 }
 
 }
